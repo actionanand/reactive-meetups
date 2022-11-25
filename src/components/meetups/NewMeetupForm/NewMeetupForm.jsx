9f5cfdbc98470299dc528e0b75fd3ec4fa8919cb
Anand raja <meetanandns@gmail.com>
2022-11-25 06:29:08 +0000

    post req

---
+++ src/components/meetups/NewMeetupForm/NewMeetupForm.jsx
@@ -0,0 +1,62 @@
+import { useRef } from 'react';
+
+import Card from '../../ui/Card/Card';
+
+import classes from './NewMeetupForm.module.css';
+
+
+
+function NewMeetupForm({onAddMeetup}) {
+  const titleInputRef = useRef();
+  const imgInputRef = useRef();
+  const addressInputRef = useRef();
+  const descInputRef = useRef();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const title = titleInputRef.current.value;
+    const image = imgInputRef.current.value;
+    const description = descInputRef.current.value;
+    const address = addressInputRef.current.value;
+
+    const meetupdata = {
+      title,
+      image,
+      description,
+      address
+    };
+
+    onAddMeetup(meetupdata)
+
+  }
+
+  return <Card>
+    <form className={classes.form} onSubmit={submitHandler}>
+      <div className={classes.control}>
+        <label htmlFor='title'> Meetup Title </label>
+        <input id='title' type='text' required ref={titleInputRef} />
+      </div>
+
+      <div className={classes.control}>
+        <label htmlFor='image'> Meetup Image </label>
+        <input id='image' type='url' required ref={imgInputRef} />
+      </div>
+
+      <div className={classes.control}>
+        <label htmlFor='address'> Meetup Address </label>
+        <input id='address' type='text' required ref={addressInputRef} />
+      </div>
+
+      <div className={classes.control}>
+        <label htmlFor='description'> Meetup Description </label>
+        <textarea id='description' rows='5' required ref={descInputRef} ></textarea>
+      </div>
+
+      <div className={classes.actions}>
+        <button> Add Meetup </button>
+      </div>
+    </form>
+  </Card>;
+}
+
+export default NewMeetupForm;
--- src/pages/NewMeetups.jsx
+++ src/pages/NewMeetups.jsx
@@ -1,5 +1,30 @@
+import { useNavigate } from 'react-router-dom';
+
+import NewMeetupForm from '../components/meetups/NewMeetupForm/NewMeetupForm';
+
+const meetupUrl = 'https://vue-http-exmp-default-rtdb.firebaseio.com/';
+
+
 function NewMeetupsPage() {
-  return <div>New Meetups!</div>
+  const navigate = useNavigate();
+  
+  const addMeetupHandler = meetup => {
+    fetch(meetupUrl + 'reactive-Meetups.json', 
+    {
+      method: 'POST',
+      body: JSON.stringify(meetup),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(() => {
+      navigate('/', { replace: true });
+    });
+  }
+
+  return <section>
+    <h1>Add New Meetup</h1>
+    <NewMeetupForm onAddMeetup={addMeetupHandler} />
+  </section>;
 }
 
 export default NewMeetupsPage;
