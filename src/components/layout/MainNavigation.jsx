901979d3547995b698a63bfe5c9ad76dcc2d54bd
Anand raja <meetanandns@gmail.com>
2022-11-25 11:21:01 +0000

    finished the proj

--- src/components/layout/MainNavigation.jsx
+++ src/components/layout/MainNavigation.jsx
@@ -1,8 +1,18 @@
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 
 import classes from './MainNavigation.module.css';
+import FavoriteContext from '../../store/favorites.context';
 
 function MainNavigation() {
+  const favCtx = useContext(FavoriteContext);
+
+  let badgeJsx;
+
+  badgeJsx = !favCtx.toatalfav ? '' 
+    : badgeJsx = <span className={classes.badge}>{favCtx.toatalfav }</span>;
+
+
   return <header className={classes.header} >
     <div className={classes.logo} > Meetups </div>
     <nav>
@@ -14,7 +24,10 @@ function MainNavigation() {
           <Link to='new-meetup'> Add New Meetup </Link>
         </li>
         <li>
-          <Link to='favorites'> My Favorites </Link>
+          <Link to='favorites'> 
+            My Favorites 
+            {badgeJsx}
+          </Link>
         </li>
       </ul>
     </nav>
